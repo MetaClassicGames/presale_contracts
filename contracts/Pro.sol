47127e2233c1adf6c322842d9ba0408ec488381e
Cristian <cristianrisueo@gmail.com>
2022-03-16 19:06:54 +0100

    Contratos base creados

---
+++ contracts/Pro.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/**
+ * @title Presale Ed.0 pro
+ * @author EscuelaCryptoEs
+ * @notice This is the contract of the preasale of the Pro NFT
+ */
+contract Pro is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
+    Counters.Counter private _tokenIdCounter;
+
+    /// @notice This is the contructor of the contract
+    constructor() ERC721("ProEd0", "PRE0") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    /**
+     * @notice this function returns the URI of the json of the contract
+     * @dev json holded on the hosting of the client
+     * @return baseURI the URI of the json
+     */
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://www.metaclassicgames.com/tokens/ed0/2.json";
+    }
+
+    /// @notice Function for pause and unpause the contract
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    /**
+     * @notice This is the function that mints the contract to an address
+     * @param to the address of the minter 
+     */
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    /**
+     * @notice Gives a security measure to not loosing the NFT
+     * @param from address of the owner
+     * @param to address of the minter
+     * @param tokenId id of the NFT to mint
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // Overrided function
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
