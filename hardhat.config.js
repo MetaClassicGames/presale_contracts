f8d67eba70d2f5a68dab2b32e3616ad15562204f
Cristian <cristianrisueo@gmail.com>
2022-04-07 17:32:56 +0200

    Contratos subidos a testnet y verificados
    RookieEd0: 0xEcb4253D7d228cB5f05F4073001DFD51DB764fF8
    
    AmateurEd0: 0x1ee0eDa3fdDAAF4484F02A35664F13590CeFEFA9
    
    ProEd0: 0x31eF5dA942dBFBB90c2b4Bf8EBBb564D59c36Ec3
    
    LegendEd0: 0xaa46511C003239F23De291Ca3917434F29e68DA1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+const apiKey = process.env.ETHERSCAN_API_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,32 +24,44 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    compilers : [
+    compilers: [
       {
-        version : "0.8.13",
-        settings : {
+        version: "0.8.13",
+        settings: {
           optimizer: {
-            enabled: true
-          }
-        }
+            enabled: true,
+          },
+        },
       },
       //Stable Coin
       {
-        version : "0.6.12",
-        settings : {
-          optimizer : {
-            enabled : true
-          }
-        }
-      }
-    ]
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
+      },
+    ],
   },
-  gasReporter : {
+  networks: {
+    matic: {
+      url: `https://rpc-mumbai.maticvigil.com`,
+      accounts: [privateKey],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: [apiKey],
+    },
+  },
+  gasReporter: {
     enabled: process.env.REPORT_GAS,
-    currency : "EUR",
-    token : "MATIC",
-    gasPriceApi : "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice", 
-    showTimeSpent : true,
-    coinmarketcap : process.env.GAS_KEY
-  }
+    currency: "EUR",
+    token: "MATIC",
+    gasPriceApi:
+      "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
+    showTimeSpent: true,
+    coinmarketcap: process.env.GAS_KEY,
+  },
 };
